34edd2fa7728f265133e6726a1dafef91a083f2b
Tai Phan <ptai7722@gmail.com>
2024-01-19 14:14:57 -0600

    updated the comments for app.engine

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express =require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+//
 const { engine:expressHandlebars } = require('express-handlebars');
 const connectDB = require('./config/db.js');
 
@@ -20,7 +21,7 @@ if (process.env.NODE_ENV === 'development') {
 
 //Setting up our view engine, which we will be using handlebars as the template engine in this project, brought in by the express-handlebars package.
 
-//Using app.engine() we will specify that any files with the extension hbs will be processed by the Handlebars template engine, passing into the expressHandlebars function an optional options object.
+//Using app.engine() we will specify that any files with the extension hbs will be processed by the Handlebars template engine, passing into it the expressHandlebars factory function and an optional configs object. This will produce the engine() function being registered for this file extension.
 app.engine('.hbs', expressHandlebars({ defaultLayout: 'main' ,extname: '.hbs' }));
 //With the set method of the app, we can specify settings for our server. We can set really any key value pair we want, but there are certain names when set, like 'view engine' below, which can be used to configure specific behaviors of our express server.
 app.set('view engine', '.hbs');
