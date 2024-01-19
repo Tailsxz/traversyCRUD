16e7436f9b48a4480f37bca2d5f3d5c05e0b3f0d
Tai Phan <ptai7722@gmail.com>
2024-01-19 14:40:49 -0600

    Set up the login.hbs layout, set up our route to serve this layout when requests for the login page(the root route "/") are made

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,10 @@ const router = express.Router();
 //@desc Login/Landing page
 //@route GET /
 router.get('/', (req, res) => {
-  res.render('login');
+  res.render('login', {
+    //This is the optional options object, from which we specify to use a layout other than our default which we set within the configs object on the app.engine call.
+    layout: 'login',
+  });
 })
 
 //@desc Dashboard
