5cc4200e2d2e25c5d3add15ccccf5b4db6b2a124
Tai Phan <ptai7722@gmail.com>
2024-01-19 13:48:09 -0600

    Brought in morgan and conditionally using it only in development mode, specified by the cross-env global environment variable within our start and dev scripts

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express =require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const expressHandlebars = require('express-handlebars');
 const connectDB = require('./config/db.js');
 
 // Loading the config for our environment variables
@@ -10,6 +12,11 @@ connectDB();
 
 //Initializing the express app server instance
 const app = express();
+
+//Here we are using the morgan middleware only if the current env variable NODE_ENV is equal to development, indicating that we are running our server in development mode with nodemon. This is so we only use morgan(a request logger middleware) in development mode.
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 //Setting up our port to be either the environment variable PORT or if it doesn't exist use 3000
 const PORT = process.env.PORT || 3000;
