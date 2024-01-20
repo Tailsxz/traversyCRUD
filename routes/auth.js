79ea91409b9cba61fff081c72b3ee0c7a48713af
Tai Phan <ptai7722@gmail.com>
2024-01-20 15:00:52 -0600

    refactored the logout route, read passport docs, post is recommended and logout now requires a callback

--- routes/auth.js
+++ routes/auth.js
@@ -13,14 +13,17 @@ router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
 //This is essentially the redirect upon successful authentication, the second options object defines a failure redirect which will redirect to the root route upon authentication failure, else run the callback which redirects the user to the dashboard path.
 router.get('/google/callback', passport.authenticate('google', { failureRedirect:'/'}), (req, res) => {
   res.redirect('/dashboard');
-})
+});
 
 //@desc Logout route, will be hit when the user clicks logout
 //@route /auth/logout
-router.get('/logout', (req, res) => {
+router.post('/logout', (req, res, next) => {
   //logout() is a passport primitive! Abstraction goes crazy
-  req.logout()
-  res.redirect('/')
-})
+  req.logout(err => {
+    if (err) return next(err);
+    res.redirect('/');
+  });
+  // res.redirect('/') <- logout requires we pass a callback now, also changing our get request to a post in order to prevent accidental or malicious logouts.
+});
 
 module.exports = router
