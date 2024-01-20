8a53d81a6b29b9cd6092f5805bd7fe7800c525c0
Tai Phan <ptai7722@gmail.com>
2024-01-20 14:48:07 -0600

    added comments to passport.js to further explain what is happening for future reference

--- config/passport.js
+++ config/passport.js
@@ -10,6 +10,7 @@ module.exports = function (passport) {
   },//the name for the last argument "done", is arbitrary and can be named anything, it is the callback function passport.js passes in for us to call and pass the user back to passport upon successful auth.
   async (accessToken, refreshToken, profile, done) => {
     console.log(profile)
+    //Creating a newUser object to be able to store them into our database if they didn't exist, in the format defined within the User schema.
     const newUser = {
       googleId: profile.id,
       displayName: profile.displayName,
@@ -17,12 +18,12 @@ module.exports = function (passport) {
       lastName: profile.name.familyName,
       image: profile.photos[0].value,
     }
-    try {
+    try {//First we try to find a user with the googleId property that matches the current profile for this verfiy callback function call.
       let user = await User.findOne({ googleId: profile.id});
-
+      //if they exist, we call done() non native passport cb to pass in the user to be stored in the session.
       if (user) {
         done(null, user);
-      } else {
+      } else {//if they do not exist, we store them into the database then call the non native passport cb.
         user = await User.create(newUser);
         done(null, user);
       }
@@ -32,7 +33,7 @@ module.exports = function (passport) {
   }));
   //we are exporting the functions that are serializing the user data storing it into the session, the nextTick queue, is essentially the promise jobs queue which we are identifying by the fact that it is ran before allowing the event loop to continue and that it is executed to completion. Turns out we were wrong about the nextTick queue! It is very similar to the promiseJobs queue, but the main difference is in the priority. The nextTick queue gets priority over the promise jobs queue!
   passport.serializeUser((user, done) => process.nextTick(() => {
-      return done(null, {
+      return done(null, {//specifying we only want to store the id,username and picture of the user into the session store.
         id: user.id,
         username: user.username,
         picture: user.picture
@@ -46,6 +47,7 @@ module.exports = function (passport) {
   // });
   //The code from above doesn't work anymore, mongoose has changed! Now the findById method only takes in an id, no longer accepting a callback to run when it is found, so instead we are checking if the user exists, and if so call the done function, which is being passed in.
   passport.deserializeUser((id, done) => {
+    //rather than deserializing from the session store, we are doing so from the MongoDB collection users.
     const user = User.findById(id);
     if (user) {
       done(null, user);
