1f2b247876b34e00be80c427894ad1c63123cc31
Tai Phan <ptai7722@gmail.com>
2024-01-22 14:14:18 -0600

    created another router for stories and brought it into our app.js

--- app.js
+++ app.js
@@ -68,7 +68,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 //Connecting to all of our routers
 app.use('/', require('./routes/index'));
-app.use('/auth', require('./routes/auth.js'));
+app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
 
 //Setting up our port to be either the environment variable PORT or if it doesn't exist use 3000
 const PORT = process.env.PORT || 3000;
---
+++ routes/stories.js
@@ -0,0 +1,18 @@
+// Routes for any requests not being made to the /auth or /stories path.
+const express = require('express');
+const router = express.Router();
+const { ensureAuth } = require('../middleware/auth');
+
+const Story = require('../models/Story');
+
+//@desc Show add story page, which will render the add.hbs body we just created.
+//@route GET /stories/add
+
+router.get('/add', ensureAuth, (req, res) => {
+  //rendering the handlebars file bringing in the add.hbs file as the body for our main layout.
+  res.render('stories/add', {
+    layout: 'login',
+  });
+})
+
+module.exports = router
